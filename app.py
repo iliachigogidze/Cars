fdfd61e76166105d0d9069312577dc6f4e7b978d
Ilia Chigogidze <iliachigogidze@gmail.com>
2018-09-20 16:56:47 +0400

    ee

--- app.py
+++ app.py
@@ -103,8 +103,9 @@ weights_path = '/trained_model/mask_rcnn_plate_0010.h5'
 # Load weights
 print("Loading weights... ", weights_path)
 print('OS Dirrrrrrrrrrrrrrrrrrrrrrrrrrrrr: ', os.path)
-print(os.path.join(weights_path, '/trained_model/mask_rcnn_plate_0010.h5')
-model.load_weights(os.path.join(weights_path, '/trained_model/mask_rcnn_plate_0010.h5'), by_name=True)
+print(os.path.join(weights_path, '/trained_model/mask_rcnn_plate_0010.h5'))
+w_path = os.path.join(weights_path, '/trained_model/mask_rcnn_plate_0010.h5')
+model.load_weights(os.path.join(w_path, by_name=True)
 
 
 @app.route('/hello', methods=['POST'])
